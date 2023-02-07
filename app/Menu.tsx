907be3b8076ea4e53dec835919e37a749954f3d0
n8zzle <playwithn8zzle@gmail.com>
2023-02-08 00:04:13 +0100

    want to create component which will help me to create qr code

--- app/Menu.tsx
+++ app/Menu.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Logo from "../public/Logo.png";
 //Libraries
 import React from "react";
+import Link from "next/link";
 
 type Props = {};
 
@@ -11,18 +12,23 @@ const Menu = (props: Props) => {
   return (
     <div className="flex flex-col bg-red-100 w-96">
       {/*Logo*/}
-      <div className="flex flex-col w-full p-10">
-        <h1 className="text-center text-3xl font-extrabold">Monkey Note</h1>
-      </div>
+      <Link href="../">
+        <div className="flex flex-col w-full p-10">
+          <h1 className="text-center text-3xl font-extrabold">Monkey</h1>
+        </div>
+      </Link>
 
       <div className="flex flex-col w-full p-10">
-        <button
-          className="bg-red-300 py-2 rounded-full"
-          onClick={() => alert("New Note")}
+        <Link
+          href="./qrcode/
+        "
         >
-          New Note
-        </button>
-        <h3 className="my-10 font-bold">All Collections</h3>
+          <input
+            type="button"
+            value="QR Code"
+            className="rounded-full bg-blue-500 p-3 w-full"
+          />
+        </Link>
       </div>
     </div>
   );
